033b0efcfd95a77c0669bf979dfb593144422307
Avi <avimahalingpure10@gmail.com>
2024-07-22 14:34:23 +0530

    add github in contacts

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Contact.css'
 import { CiLinkedin } from "react-icons/ci";
 import { BiLogoGmail } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
+
 
 const Contact = () => {
   return (
@@ -18,6 +20,7 @@ const Contact = () => {
             <div className="contacts">
                 <a href="https://in.linkedin.com/in/avi-mahalingpure-640248280"><CiLinkedin /></a>
                 <a href="mailto:avimahalingpure10@gmail.com"><BiLogoGmail /></a>
+                <a href="https://github.com/Spikree"><FaGithub /></a>
             </div>
         </div>
     </div>
